56a2da88d9df73178cc12f4a565b0dd56b5ea298
BrianJakobs <31827219+BrianJakobs@users.noreply.github.com>
2019-05-21 01:33:16 +0200

    V5
    Kleuren zijn veranderd om meer met de poster in overeenstemming te komen. Ook zijn er drie input velden toegevoegd, twee range sliders en één kleur input veld. Het kleuren input veld is al werkende waarme de gebruiker alle rode kleuren kan aanpassen naar een eigen kleur.
    
    Daarnaast is de svg aangevuld met een tweede svg welke een kleur heeft en onder de originele svg geplaatst is. Deze svg veranderd ook van kleur door de input.
    
    Css variabele zijn gebruikt voor de input velden waaronder het kleuren input veld. De headturn is nu niet meer op basis van een hover, maar een click.

--- js/main.js
+++ js/main.js
@@ -1,18 +1,46 @@
-//var head = document.querySelector("svg #man #man-head");
-//
-//document.onmmousemove = function(){
-//    var x = event.clientX + 100 / window.innerWidth + "%";
-//    var y = event.clientY + 100 / window.innerHeight + "%";
-//    
-//    head.style.left = x;
-//    head.style.top = y;
-//    head.style.transform = "translate( -"+x+", -"+y+")";
-//}
-var head = document.querySelector("svg #man #head");
-var headArea = document.querySelector("svg #man #head-fill");
+var head = document.querySelector("svg #man #head"),
+    headArea = document.querySelector("svg #man #head-fill"),
+    headArea = document.querySelector("svg #man #ca-head"),
+    manArea = document.querySelector("svg #man-fill"),
+    manStrokes = document.querySelector("svg #man g"),
+    womanArea = document.querySelector("svg #woman-fill"),
+    womanStrokes = document.querySelector("svg #woman g"),
+    flowersArea = document.querySelector("svg #flowers-fill"),
+    flowersStrokes = document.querySelector("svg #flowers g"),
+    carArea = document.querySelector("svg #car-fill"),
+    carStrokes = document.querySelector("svg #car g"),
+    
+//    https://developer.mozilla.org/en-US/docs/Web/API/Window/getComputedStyle
+    dashArraySlider = document.querySelector(".controls .sliders-container #dashArray"),
+    strokes = document.querySelector("svg g"),
+    
+//    https://vanseodesign.com/css/custom-properties-and-javascript/
+    strokeStyles = window.getComputedStyle(strokes),
+    dashArrayValue = strokeStyles.getPropertyValue("--dasharray-num)"),
+    headerStyles = window.getComputedStyle(document.querySelector(".textbox h1")),
+    colorValue  = headerStyles.getPropertyValue("--color"),
+    colorPick = document.querySelector(".controls .sliders-container #colorPick"),
+    headerOne = document.querySelector("body");
 
-headArea.addEventListener('mouseover', function() {
-    console.log("he");
-    head.classList.add("flipHead");
-    headArea.classList.add("flipHeadFill");
+
+colorPick.addEventListener("change", function() {
+    headerOne.style.setProperty("--color",this.value);
+})
+
+manArea.addEventListener('click', function() {
+    console.log("test");
+    head.classList.toggle("flipHead");
+    headArea.classList.toggle("flipHeadFill");
+    headCA.classList.toggle("flipHeadFill");
 });
+
+manArea.addEventListener('click', function() {
+    console.log("stop");
+    manStrokes.classList.add("stop-animation");
+});
+
+womanArea.addEventListener('click', function() {
+    womanStrokes.classList.add("stop-animation");
+});
+
+strokeStyles.setProperty(dashArrayValue, dashArraySlider.nodeValue);
